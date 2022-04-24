4ea988086e648d181ca8e55c928ea34971f25db7
ShekChun Lee <leeshekchun@gmail.com>
2022-04-23 19:22:27 -0600

    add html structure and global variables in js

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+let inputSearch = document.querySelector(".search-bar")
+let cityName = document.querySelector("#city")
+let searchHistory = document.querySelector("#city-history")
+let rightContainerTop = document.querySelector(".right-top")
+let rightContainerBottom = document.querySelector(".right-bottom")
